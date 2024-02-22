1d1b3315b79bb1deb034bcf471b98a2ef7e731f8
lapnd <lapd@viosoft.com>
2024-02-22 15:55:11 +0700

    Update install-go.sh

--- install-go.sh
+++ install-go.sh
@@ -1,28 +1,42 @@
 # codenoid
 # https://gist.github.com/codenoid/4806365032bb4ed62f381d8a76ddb8e6
-printf "Checking latest Go version...\n";
-LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text | head -n 1)";
+# Function to check if a variable is set in the ~/.bashrc file
+check_variable() {
+    grep -q "$1" ~/.bashrc
+}
+
+printf "Checking latest Go version...\n"
+LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text | head -n 1)"
 LATEST_GO_DOWNLOAD_URL="https://go.dev/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz"
 
-printf "cd to home ($USER) directory \n"
-cd $HOME
+printf "cd to home ($USER) directory\n"
+cd "$HOME"
 
-printf "Downloading ${LATEST_GO_DOWNLOAD_URL}\n\n";
-curl -OJ -L --progress-bar $LATEST_GO_DOWNLOAD_URL
+printf "Downloading ${LATEST_GO_DOWNLOAD_URL}\n\n"
+curl -OJ -L --progress-bar "$LATEST_GO_DOWNLOAD_URL"
 
 printf "Extracting file...\n"
-tar -xf ${LATEST_GO_VERSION}.linux-amd64.tar.gz
+tar -xf "${LATEST_GO_VERSION}.linux-amd64.tar.gz"
 
-export GOROOT="$HOME/go"
-export GOPATH="$HOME/go/packages"
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+# Check and set GOROOT if not already set
+if ! check_variable "export GOROOT="; then
+    echo "export GOROOT=\"$HOME/go\"" >> ~/.bashrc
+fi
 
-printf '⚠️  ADD (OR REPLACE) THIS LINE BELOW TO YOUR ~/.bashrc  ⚠️
+# Check and set GOPATH if not already set
+if ! check_variable "export GOPATH="; then
+    echo "export GOPATH=\"$HOME/go/packages\"" >> ~/.bashrc
+fi
 
-export GOROOT="$HOME/go"
-export GOPATH="$HOME/go/packages"
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-\n'
+# Check and set PATH if not already set
+if ! check_variable "export PATH=\$PATH:\$GOROOT/bin:\$GOPATH/bin"; then
+    echo "export PATH=\$PATH:\$GOROOT/bin:\$GOPATH/bin" >> ~/.bashrc
+fi
 
-printf "You are ready to Go!";
+printf '⚠️  ADD (OR REPLACE) THIS LINE BELOW TO YOUR ~/.bashrc  ⚠️\n\n'
+printf "export GOROOT=\"$HOME/go\"\n"
+printf "export GOPATH=\"$HOME/go/packages\"\n"
+printf "export PATH=\$PATH:\$GOROOT/bin:\$GOPATH/bin\n\n"
+
+printf "You are ready to Go!\n"
 go version
